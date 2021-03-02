19231a34bd69f39504e4f2357d4a001c09f7a76a
Willi Nardo <56558752+wind-create@users.noreply.github.com>
2021-03-02 12:39:36 +0700

    Add files via upload

---
+++ program.py
@@ -0,0 +1,12 @@
+a = int(input("masukkan nilai a = "))
+b = int(input("masukkan nilai b = "))
+
+c = a + b
+d = a - b
+e = a * b
+f = a / b
+
+print(f"Penjumlahan a + b = {c}")
+print(f"Pengurangan a - b = {d}")
+print(f"Perkalian   a * b = {e}")
+print(f"Pembagian   a / b = {f}")
---
+++ program_2.py
@@ -0,0 +1,39 @@
+def penjumlahan(a,b):
+    return a + b
+
+def pengurangan(a,b):
+    return a - b
+
+def perkalian(a,b):
+    return a * b
+
+def pembagian(a,b):
+    return a / b
+
+while True:
+    print("Menu: ")
+    print("1. Penjumlahan")
+    print("2. Pengurangan")
+    print("3. Perkalian")
+    print("4. Pembagian")
+    menu = int(input("pilihan: "))
+    if menu == 1:
+        a = int(input("masukkan nilai a = "))
+        b = int(input("masukkan nilai b = "))
+        c = penjumlahan(a,b)
+        print(f"Hasil dari penjumlahan a + b = {c}")
+    elif menu == 2:
+        a = int(input("masukkan nilai a = "))
+        b = int(input("masukkan nilai b = "))
+        c = pengurangan(a,b)
+        print(f"Hasil dari pengurangan a - b = {c}")
+    elif menu == 3:
+        a = int(input("masukkan nilai a = "))
+        b = int(input("masukkan nilai b = "))
+        c = perkalian(a,b)
+        print(f"Hasil dari perkalian a * b = {c}")
+    elif menu == 4:
+        a = int(input("masukkan nilai a = "))
+        b = int(input("masukkan nilai b = "))
+        c = pembagian(a,b)
+        print(f"Hasil dari pembagian a / b = {c}")
